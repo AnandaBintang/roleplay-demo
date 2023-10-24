2e7006ebbbc2b4be799fd866327de243159a1d5e
AnandaBintang <anandabintang4@gmail.com>
2023-10-24 14:04:51 +0700

    some animation

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
       preloader.remove();
     }, 500);
   }
+
+  const text = "We are a Digital Agency that loves creating Animation Videos";
+  const targetElement = document.getElementById("animation-target");
+
+  function typeText(index) {
+    if (index < text.length) {
+      targetElement.textContent += text.charAt(index);
+      setTimeout(typeText, 75, index + 1);
+    }
+  }
+
+  typeText(0);
 });
